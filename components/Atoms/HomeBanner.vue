1c9b958ba33616fec8af802803517092a51d2dd6
NyanBot <laville.geoffrey@gmail.com>
2023-02-14 18:00:50 +0100

    Modify home-banner full screen

--- components/Atoms/HomeBanner.vue
+++ components/Atoms/HomeBanner.vue
@@ -1,6 +1,6 @@
 <template>
     <div>   
-        <img src="../../static/bg-van.jpeg" alt="Van life" class="object-cover w-full max-h-52 sm:max-h-72 md:max-h-96">    
+        <img src="../../static/bg-van.jpeg" alt="Van life" class="absolute top-0 left-0 object-cover w-full h-screen">    
     </div>    
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,11 @@
   <div>
     <nav-header />
     <home-banner />
-    <div class="text-center p-10">
-      <h2 class="text-3xl font-bold mb-8">
+    <div class="absolute text-center m-auto w-full">
+      <h2 class="font-bold text-4xl text-gray-700 mt-16 md:mt-24 mb-12 px-10">
         Partez à la découverte du monde <br />
-        louer le van idéal pour votre aventure avec Yespapa !
+        louer le van idéal pour votre aventure<br/> 
+        avec Yespapa !
       </h2>
       <button-primary text="Je découvre" link="/vans" />
     </div>
